2808c772e13105e0ca20eda57b5932e22eeee456
SabinSnider <nepalisoloplayer@gmail.com>
2019-01-09 16:15:02 +0545

    Initial commit

---
+++ jQueryDataTablev2/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using jQueryDataTablev2.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace jQueryDataTablev2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult loadData()
+        {
+
+            using (DatabaseDataTableEntities entities = new DatabaseDataTableEntities())
+            {
+                var data = entities.Customers.OrderBy(a => a.CompanyName).ToList();
+                return Json(new { data = data }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+    }
+}
