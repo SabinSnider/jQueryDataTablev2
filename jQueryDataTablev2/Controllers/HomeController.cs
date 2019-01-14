8aa38800e7ea8534f02afe0287855add881f53b0
SabinSnider <nepalisoloplayer@gmail.com>
2019-01-14 16:07:10 +0545

    Runs full CRUD in dataTable

--- jQueryDataTablev2/Controllers/HomeController.cs
+++ jQueryDataTablev2/Controllers/HomeController.cs
@@ -1,29 +1,125 @@
 ﻿using jQueryDataTablev2.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 
 namespace jQueryDataTablev2.Controllers
 {
+
+
     public class HomeController : Controller
     {
+
+        DatabaseDataTableEntities2 context = new DatabaseDataTableEntities2();
         // GET: Home
+
         public ActionResult Index()
+        {
+
+            List<Customers1> customer = context.Customers1.ToList();
+
+            return View(customer);
+        }
+
+
+
+
+        // GET: Customer/Create
+        public ActionResult Create()
         {
             return View();
         }
-
-
-        public ActionResult loadData()
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(Customers1 customer)
         {
-
-            using (DatabaseDataTableEntities entities = new DatabaseDataTableEntities())
+            try
             {
-                var data = entities.Customers.OrderBy(a => a.CompanyName).ToList();
-                return Json(new { data = data }, JsonRequestBehavior.AllowGet);
+                using (DatabaseDataTableEntities2 dbModel = new DatabaseDataTableEntities2())
+                {
+                    dbModel.Customers1.Add(customer);
+                    dbModel.SaveChanges();
+                }
 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+
+                return View();
+            }
+
+        }
+
+
+
+        // GET: Edit
+        public ActionResult Edit(int id)
+        {
+            using (DatabaseDataTableEntities2 dbModel = new DatabaseDataTableEntities2())
+            {
+                return View(dbModel.Customers1.Where(x => x.CustomerID == id).FirstOrDefault());
             }
         }
 
+        [HttpPost]
+        public ActionResult Edit(int id, Customers1 customer)
+        {
+            try
+            {
+                using (DatabaseDataTableEntities2 dbModel = new DatabaseDataTableEntities2())
+                {
+                    dbModel.Entry(customer).State = EntityState.Modified;
+                    dbModel.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+
+
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (DatabaseDataTableEntities2 dbModels = new DatabaseDataTableEntities2())
+            {
+                return View(dbModels.Customers1.Where(x => x.CustomerID == id).FirstOrDefault());
+            }
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                using (DatabaseDataTableEntities2 dbModel = new DatabaseDataTableEntities2())
+                {
+                    Customers1 customers = dbModel.Customers1.Where(x => x.CustomerID == id).FirstOrDefault();
+                    dbModel.Customers1.Remove(customers);
+                    dbModel.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+
+        public ActionResult Details(int id)
+        {
+            using (DatabaseDataTableEntities2 dbModel = new DatabaseDataTableEntities2())
+            {
+                return View(dbModel.Customers1.Where(x => x.CustomerID == id).FirstOrDefault());
+            }
+
+        }
 
     }
 }
